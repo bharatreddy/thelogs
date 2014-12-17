52b4c213a912e919b97e84a27e0859631033b3e9
Bharat Kunduri <bharatr@vt.edu>
2014-12-16 19:18:34 -0800

    user reg work

--- bear/models.py
+++ bear/models.py
@@ -3,20 +3,19 @@ from werkzeug import generate_password_hash, check_password_hash
 # set up the DB
 dbAlc = SQLAlchemy()
 # create a user class
-class User(db.Model):
+class User(dbAlc.Model):
     __tablename__ = 'Users'
-    userid = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(120), unique=True)
-    pwdhash = db.Column(db.String(54))
-    def __init__(self, name, lastname, email, password):
+    userid = dbAlc.Column(dbAlc.Integer, primary_key = True)
+    name = dbAlc.Column(dbAlc.String(100))
+    email = dbAlc.Column(dbAlc.String(120), unique=True)
+    pwdhash = dbAlc.Column(dbAlc.String(54))
+    def __init__(self, name, email, password):
         self.name = name.title()
-        self.lastname = lastname.title()
         self.email = email.lower()
         self.set_password(password)
     # generate a password hash 
     def set_password(self, password):
         self.pwdhash = generate_password_hash(password)
-    # verify password
+    # verify password in subsequent logins
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
--- bear/routes.py
+++ bear/routes.py
@@ -4,7 +4,7 @@ from forms import RegistrationForm
 from flask.ext.mail import Message, Mail
 import MySQLdb
 import json
-from models import dbAlc
+from models import dbAlc, User
 
 @app.route("/")
 def hello():
@@ -21,12 +21,22 @@ def regularpage( pagename=None ):
 def contact():
     form = RegistrationForm()
     # if a GET request is sent to the server, 
-    # the web page containing the form should be retrieved 
+    # the web page containing the form should be retrieved
     # and loaded in browser. If the server receives a POST request,
     # a function should capture the form field data and check if it's valid.
     if request.method == 'POST':
-      return 'Form posted.'
-    return render_template('contact.html', form=form)
+        if form.validate() == False:
+            return render_template('contact.html', form=form)
+        else:
+            # Nothing wrong with the form.
+            # Create a new user object
+            newUser = User(form.name.data, form.email.data, form.password.data)
+            # Add the user to the DB
+            dbAlc.session.add(newUser)
+            dbAlc.session.commit()
+            return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
 
 # if __name__ == "__main__":
 #     app.debug=True
