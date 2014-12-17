6592fafab28bb390e10dc37f0ee1a3f5d8c02bad
Bharat Kunduri <bharatr@vt.edu>
2014-12-16 22:33:51 -0800

    profile page

--- bear/routes.py
+++ bear/routes.py
@@ -1,5 +1,5 @@
 from bear import app
-from flask import Flask, render_template, request, jsonify, flash
+from flask import Flask, render_template, request, jsonify, flash, session, redirect, url_for
 from forms import RegistrationForm
 from flask.ext.mail import Message, Mail
 import MySQLdb
@@ -34,9 +34,31 @@ def contact():
             # Add the user to the DB
             dbAlc.session.add(newUser)
             dbAlc.session.commit()
-            return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+            session['email'] = newUser.email
+            # If all is good get the profile page of the user
+            return redirect(url_for('profile'))
     elif request.method == 'GET':
         return render_template('contact.html', form=form)
+
+@app.route("/profile") 
+def profile(): 
+    # fucntion to access the profile page of the user
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+    # Check if the user is in our db
+    user = User.query.filter_by(email = session['email']).first()
+    dbRaw = \
+    MySQLdb.connect( user='root', host='localhost', port=3306, db='Logbook' )
+    # check if the email-id is already taken
+    queryUserChk = " SELECT name FROM Users WHERE email = " \
+    + "'" + session['email'] + "'"
+    dbRaw.query( queryUserChk )
+    userDetails = dbRaw.store_result().fetch_row( maxrows=0 )
+    userName = userDetails[0][0]
+    if userDetails is None:
+        return redirect(url_for('signin'))
+    else:
+        return render_template('profile_layout.html', profileName=userName)
 
 # if __name__ == "__main__":
 #     app.debug=True
