4681b8e1ed8742bfe920bc1eede67ea527be2d9e
Bharat Kunduri <bharatr@vt.edu>
2014-12-17 22:24:59 -0800

    get symbs

--- scripts/listedStocks.py
+++ scripts/listedStocks.py
@@ -6,10 +6,45 @@ class GetListedStocks(object):
 
     def __init__(self):
         import string
-        # get the base urls from rediff for both bse and nse
-        self.baseUrlBse = 'http://money.rediff.com/companies/'
-        self.baseUrlNse = 'http://money.rediff.com/companies/nseall/'
-        # the stock names are order by alphabets
-        alphabetList = string.ascii_lowercase
-        for j in alphabetList:
-            print j
+        # get the base urls from rediff
+        self.baseUrl = 'http://money.rediff.com/companies/nseall/'
+
+    def get_all_urls(self):
+        # Now the urls are paginated so we need all the urls.
+        # We'll keep them in a list.
+        import bs4
+        import urllib2
+        # soupify
+        urlData = urllib2.urlopen(self.baseUrl).read()
+        soup = bs4.BeautifulSoup(urlData)
+        # the pagination is present in the table with 
+        # the class named "pagination-container-company"
+        paginationTab = soup.findAll( \
+            attrs={'class': "pagination-container-company"} )
+        pageTags = paginationTab[0].findAll('td')
+        # pageTags = pageTags.findAll('a').text
+        print pageTags
+
+
+    def get_stock_symbols(self):
+        # we'll use BeautifulSoup for scraping rediff
+        import bs4
+        import urllib2
+        # soupify
+        urlData = urllib2.urlopen(self.baseUrl).read()
+        soup = bs4.BeautifulSoup(urlData)
+        # the stocks are listed in the "dataTable" table
+        stocksTab = soup.findAll(attrs={'class': "dataTable"})
+        # the <td> tags have the company information
+        tdTags = stocksTab[0].findAll('td')
+        # loop through the td tags and retreive the info
+        # Here the even numbered tags have the company info
+        # the odd numbered tags have company symbol, so we need
+        # take that into account. Store the results in a dict
+        stockDict = {}
+        for n, t in enumerate(tdTags):
+            currText = t.text
+            currText = ' '.join(currText.split())
+            if n%2 == 0 :
+                symbolText = tdTags[n+1].text
+                stockDict[currText] = symbolText
