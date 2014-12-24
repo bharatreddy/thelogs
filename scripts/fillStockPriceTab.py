e6f6e5926fe94266e710898db5a23938eb7985e5
Bharat Kunduri <bharatr@vt.edu>
2014-12-23 22:03:30 -0800

    bug fix

--- scripts/fillStockPriceTab.py
+++ scripts/fillStockPriceTab.py
@@ -22,9 +22,107 @@ class FillStockPriceTable(object):
             FROM StockSymbols
             """)
         self.cursor.execute(qrySyms)
-        self.symList = [s[0] for s in self.cursor]
+        self.allSymList = [s[0] for s in self.cursor]
+        # get a list of active stocks
+        qrySyms = ("""
+            SELECT DISTINCT stock_symbol
+            FROM stockTransactions
+            """)
+        self.cursor.execute(qrySyms)
+        self.actvSymList = [s[0] for s in self.cursor]
+
+    def update_sym_list(self):
+        # update/insert symbol list in the StockPrices table
+        for st in self.allSymList:
+            query = ("INSERT INTO StockPrices "
+                   " (stock_symbol, NSE_cost_per_unit, NSE_datetime, BSE_cost_per_unit, BSE_datetime) "
+                   " VALUES (%s, %s, %s, %s, %s) "
+                   " ON DUPLICATE KEY UPDATE "
+                   "   stock_symbol=VALUES(stock_symbol), "
+                   "   NSE_cost_per_unit=VALUES(NSE_cost_per_unit), "
+                   "   NSE_datetime=VALUES(NSE_datetime), "
+                   "   BSE_cost_per_unit=VALUES(BSE_cost_per_unit), "
+                   "   BSE_datetime=VALUES(BSE_datetime) "
+                   )
+            params = (
+                st, 
+                None, 
+                None, 
+                None, 
+                None)
+            self.cursor.execute(query, params)
+            self.conn.commit()
 
     def fill_stock_price_tab(self):
+        import stockPrice
+        import datetime
+        from pytz import timezone
+        # the date string we get has the following format
+        # 23 Dec 3:29pm
+        dtFormat = "%d %b %I:%M%p"
+        # loop through the symbols in the list and
+        # fill current stock price into the table.
+        for stsym in self.allSymList:
+            nseObj = stockPrice.GetStockPrice(stsym, exchange='NSE')
+            bseObj = stockPrice.GetStockPrice(stsym, exchange='BSE')
+            try:
+                (currNSEDate, currNSEPrice) = nseObj.get_stock_data()
+                currNSEPrice = float(currNSEPrice.replace(",", ""))
+                try:
+                    # convert the datetime string to python datetime object
+                    currNSEDate = datetime.datetime.strptime(currNSEDate, dtFormat)
+                    # need to set the year as we dont get 
+                    # the information from the string.
+                    # get the current year
+                    currYear = datetime.datetime.utcnow().year
+                    currNSEDate = currNSEDate.replace(year=2014)
+                except:
+                    # if is fails get current time in india
+                    currNSEDateUTC = datetime.datetime.now(timezone('UTC'))
+                    currNSEDate = currNSEDateUTC.astimezone(timezone('Asia/Kolkata'))
+            except:
+                (currNSEDate, currNSEPrice) = (None,None)
+            try:
+                (currBSEDate, currBSEPrice) = bseObj.get_stock_data()
+                currBSEPrice = float(currBSEPrice.replace(",", ""))
+                try:
+                    currBSEDate = datetime.datetime.strptime(currBSEDate, dtFormat)
+                    # need to set the year as we dont get 
+                    # the information from the string.
+                    # get the current year
+                    currYear = datetime.datetime.utcnow().year
+                    currBSEDate = currBSEDate.replace(year=2014)
+                except:
+                    # if is fails get current time in india
+                    currBSEDateUTC = datetime.datetime.now(timezone('UTC'))
+                    currBSEDate = currBSEDateUTC.astimezone(timezone('Asia/Kolkata'))
+            except:
+                (currBSEDate, currBSEPrice) = (None,None)
+            print stsym, currNSEDate, currNSEPrice, currBSEDate, currBSEPrice
+            # fill in the price info in the tables
+            try:
+                query = ("INSERT INTO StockPrices "
+                       " (stock_symbol, NSE_cost_per_unit, NSE_datetime, BSE_cost_per_unit, BSE_datetime) "
+                       " VALUES (%s, %s, %s, %s, %s) "
+                       " ON DUPLICATE KEY UPDATE "
+                       "   stock_symbol=VALUES(stock_symbol), "
+                       "   NSE_cost_per_unit=VALUES(NSE_cost_per_unit), "
+                       "   NSE_datetime=VALUES(NSE_datetime), "
+                       "   BSE_cost_per_unit=VALUES(BSE_cost_per_unit), "
+                       "   BSE_datetime=VALUES(BSE_datetime) "
+                       )
+                params = (
+                    stsym, 
+                    currNSEPrice, 
+                    currNSEDate, 
+                    currBSEPrice, 
+                    currBSEDate)
+                self.cursor.execute(query, params)
+                self.conn.commit()
+            except:
+                print "-------------------------INSERT FAILED-------------------------"
+
+    def active_stock_prices(self):
         import stockPrice
         import datetime
         # the date string we get has the following format
@@ -32,30 +130,62 @@ class FillStockPriceTable(object):
         dtFormat = "%d %b %I:%M%p"
         # loop through the symbols in the list and
         # fill current stock price into the table.
-        for st in self.symList:
-            nseObj = stockPrice.GetStockPrice(st, exchange='NSE')
-            bseObj = stockPrice.GetStockPrice(st, exchange='BSE')
+        for stsym in self.actvSymList:
+            nseObj = stockPrice.GetStockPrice(stsym, exchange='NSE')
+            bseObj = stockPrice.GetStockPrice(stsym, exchange='BSE')
             try:
                 (currNSEDate, currNSEPrice) = nseObj.get_stock_data()
-                # convert the datetime string to python datetime object
-                currNSEDate = datetime.datetime.strptime(currNSEDate, dtFormat)
-                # need to set the year as we dont get 
-                # the information from the string.
-                # get the current year
-                currYear = datetime.datetime.utcnow().year
-                currNSEDate = currNSEDate.replace(year=2014)
+                currNSEPrice = float(currNSEPrice.replace(",", ""))
+                try:
+                    # convert the datetime string to python datetime object
+                    currNSEDate = datetime.datetime.strptime(currNSEDate, dtFormat)
+                    # need to set the year as we dont get 
+                    # the information from the string.
+                    # get the current year
+                    currYear = datetime.datetime.utcnow().year
+                    currNSEDate = currNSEDate.replace(year=2014)
+                except:
+                    # if is fails get current time in india
+                    currNSEDateUTC = datetime.datetime.now(timezone('UTC'))
+                    currNSEDate = currNSEDateUTC.astimezone(timezone('Asia/Kolkata'))
             except:
                 (currNSEDate, currNSEPrice) = (None,None)
             try:
                 (currBSEDate, currBSEPrice) = bseObj.get_stock_data()
-                currBSEDate = datetime.datetime.strptime(currBSEDate, dtFormat)
-                # need to set the year as we dont get 
-                # the information from the string.
-                # get the current year
-                currYear = datetime.datetime.utcnow().year
-                currBSEDate = currBSEDate.replace(year=2014)
+                currBSEPrice = float(currBSEPrice.replace(",", ""))
+                try:
+                    currBSEDate = datetime.datetime.strptime(currBSEDate, dtFormat)
+                    # need to set the year as we dont get 
+                    # the information from the string.
+                    # get the current year
+                    currYear = datetime.datetime.utcnow().year
+                    currBSEDate = currBSEDate.replace(year=2014)
+                except:
+                    # if is fails get current time in india
+                    currBSEDateUTC = datetime.datetime.now(timezone('UTC'))
+                    currBSEDate = currBSEDateUTC.astimezone(timezone('Asia/Kolkata'))
             except:
                 (currBSEDate, currBSEPrice) = (None,None)
+            print stsym, currNSEDate, currNSEPrice, currBSEDate, currBSEPrice
             # fill in the price info in the tables
-            print st, currNSEDate, currNSEPrice, currBSEDate, currBSEPrice
-            break
+            try:
+                query = ("INSERT INTO StockPrices "
+                       " (stock_symbol, NSE_cost_per_unit, NSE_datetime, BSE_cost_per_unit, BSE_datetime) "
+                       " VALUES (%s, %s, %s, %s, %s) "
+                       " ON DUPLICATE KEY UPDATE "
+                       "   stock_symbol=VALUES(stock_symbol), "
+                       "   NSE_cost_per_unit=VALUES(NSE_cost_per_unit), "
+                       "   NSE_datetime=VALUES(NSE_datetime), "
+                       "   BSE_cost_per_unit=VALUES(BSE_cost_per_unit), "
+                       "   BSE_datetime=VALUES(BSE_datetime) "
+                       )
+                params = (
+                    stsym, 
+                    currNSEPrice, 
+                    currNSEDate, 
+                    currBSEPrice, 
+                    currBSEDate)
+                self.cursor.execute(query, params)
+                self.conn.commit()
+            except:
+                print "-------------------------INSERT FAILED-------------------------"
