4004cf0611c7475a600781f601d84da2a10133ac
Bharat Kunduri <bharatr@vt.edu>
2014-12-30 19:35:47 -0800

    sttw

---
+++ scripts/tweetStocks.py
@@ -0,0 +1,9 @@
+class StockTweets(objects):
+	"""
+	A python class to get tweets about stocks using 
+	the streaming API.
+	"""
+	def __init__(self):
+		# set the key and secret
+		self.apiKey = "8Ug3XPRlkM5oWka7l8dXF5WBl"
+		self.apiSecret = "zDhlhTMAFuAH8KTrR9R6zSm1h1BmDAtdW0VoAWeSWKCLr6s1Nf"
