ccb0e2814f52854ffb53bdfcf7a817c423373c31
Bharat Kunduri <bharatr@vt.edu>
2015-01-02 22:50:34 -0800

    et text

--- scripts/etmNews.py
+++ scripts/etmNews.py
@@ -2,7 +2,10 @@ if __name__ == "__main__":
     import etmNews
     etObj = etmNews.GetEcTimesNews()
     urlDict = etObj.get_urls()
-    # etObj.get_news_articles(newsList)
+    newsUrl = ''
+    if 'News' in urlDict:
+        newsUrl = urlDict['News']
+    etObj.get_url_data(newsUrl)
 
 
 class GetEcTimesNews(object):
@@ -25,7 +28,7 @@ class GetEcTimesNews(object):
         'http://economictimes.indiatimes.com/'
 
     def get_urls(self):
-        # get the urls to the news items
+        # get the urls to different menu items
         import bs4
         import urllib2
         # get the url with all the links like News, Recos etc
@@ -39,5 +42,67 @@ class GetEcTimesNews(object):
         etUrlTag = etUrlNavTag[0].findAll('a')
         urlDict = {}
         for etu in etUrlTag:
-            urlDict[etu.get_text()] = etu['href']
+            urlDict[etu.get_text()] = self.baseNewsUrl + etu['href']
         return urlDict
+
+    def get_url_data(self, newsUrl):
+        # get required data from a url
+        import bs4
+        import urllib2
+        # get the list of news articles
+        if newsUrl == '':
+            return None
+        # soupify 
+        urlData = urllib2.urlopen(newsUrl).read()
+        soup = bs4.BeautifulSoup(urlData)
+        # get the actual url of the news website
+        urlListDiv = soup.findAll( \
+            attrs={'class': "eachStory"} )
+        stockSyms, stockNames = self.get_stock_lists()
+        stockData = stockSyms + stockNames
+        # add some additional terms
+        # stockData += ['shares', 'stocks', 'industry']
+        # have a dict of relavant articles
+        rlvntArticles = {}
+        for ud in urlListDiv:
+            udTag = ud.findAll('a')
+            currUrl = self.baseNewsUrl + udTag[0]['href']
+            # soupify the current url
+            currUrlData = urllib2.urlopen(currUrl).read()
+            currSoup = bs4.BeautifulSoup(currUrlData)
+            # get the actual url of the news website
+            currUrlTextDiv = currSoup.findAll( \
+                attrs={'class': "artText"} )
+            currText = currUrlTextDiv[0].get_text()
+            if any(word in currText for word in stockData):
+                rlvntArticles[currUrl] = currText
+                print "found-->", currUrl
+            else:
+                print "None-->", currUrl
+                continue
+
+    def get_stock_lists(self):
+        # get a list of all the stocks in our database
+        import pandas
+        import mysql.connector
+        conn = mysql.connector.Connect(host='localhost',user='root',\
+                        password='',database='Logbook')
+        qryStockNames = "SELECT stocksymbol, stockname FROM StockSymbols"
+        stockListDF = pandas.read_sql( qryStockNames, conn )
+        stockNames = stockListDF['stockname'].tolist()
+        stockSyms = stockListDF['stocksymbol'].tolist()
+        return stockSyms, stockNames
+
+    def get_stock_trans_list(self):
+        # get a list of all active stocks in our database
+        import pandas
+        import mysql.connector
+        conn = mysql.connector.Connect(host='localhost',user='root',\
+                        password='',database='Logbook')
+        qryStockNames = "SELECT DISTINCT stockname FROM stockTransactions"+\
+        " INNER JOIN stockSymbols ON stock_symbol=stocksymbol;"
+        stockListDF = pandas.read_sql( qryStockNames, conn )
+        stockNames = stockListDF['stockname'].tolist()
+        return stockNames
+
+
