647be3a89f63ce96642d696f593179338bb906f4
bharatreddy <bharatr@vt.edu>
2015-02-28 21:00:44 +0530

    recos base url

--- scripts/etmRecos.py
+++ scripts/etmRecos.py
@@ -1,11 +1,12 @@
 if __name__ == "__main__":
-    import etmNews
-    etObj = etmNews.GetEcTimesRecos()
-    urlDict = etObj.get_urls()
-    newsUrl = ''
-    if 'News' in urlDict:
-        newsUrl = urlDict['News']
-    etObj.get_url_data(newsUrl)
+    import etmRecos
+    etObj = etmRecos.GetEcTimesRecos()
+    recosBaseUrl = etObj.get_recos_baseurl()
+    print recosBaseUrl
+    # newsUrl = ''
+    # if 'News' in urlDict:
+    #     newsUrl = urlDict['News']
+    # etObj.get_url_data(newsUrl)
 
 
 class GetEcTimesRecos(object):
@@ -26,3 +27,25 @@ class GetEcTimesRecos(object):
         # set the base url for economic times
         self.baseNewsUrl = \
         'http://economictimes.indiatimes.com/'
+
+    def get_recos_baseurl(self):
+        # get the urls to different menu items
+        import bs4
+        import urllib2
+        # get the url with all the links like News, Recos etc
+        infoUrl = self.baseNewsUrl + 'markets/stocks/'
+        # soupify 
+        urlData = urllib2.urlopen(infoUrl).read()
+        soup = bs4.BeautifulSoup(urlData)
+        # get the actual url of the news website
+        etUrlNavTag = soup.findAll( \
+            attrs={'id': "subSecNav"} )
+        # get the url of Recos, start with getting nav bar 
+        etUrlTag = etUrlNavTag[0].findAll('a')
+        # Check if the term Recos is present in the list
+        recosBaseUrl = None
+        for etu in etUrlTag:
+            if ("Recos" in etu['href']) or ("recos" in etu['href']):
+                return self.baseNewsUrl + etu['href']
+        # If url is not found
+        return None
