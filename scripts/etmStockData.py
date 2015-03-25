b2b30088a7adfb04c1eb6f1084d102e93864df5f
bharatreddy <bharatr@vt.edu>
2015-03-25 17:24:35 +0530

    ticker urls

--- scripts/etmStockData.py
+++ scripts/etmStockData.py
@@ -1,3 +1,9 @@
+if __name__ == "__main__":
+    import etmStockData
+    etObj = etmStockData.GetStockData()
+    tickerUrlList = etObj.get_ticker_urls()
+    print tickerUrlList
+
 class GetStockData(object):
     """
     A class to get the data about stocks from Economic times.
@@ -18,4 +24,16 @@ class GetStockData(object):
         self.baseUrl = \
         'http://economictimes.indiatimes.com/markets/stocks/stock-quotes'
     
-    
+    def get_ticker_urls(self):
+        # Now there are a large number of companies and each company
+        # is listed according to alphabets. In this function we'll 
+        # get the urls of pages which show the companies by alphabets.
+        import string
+        # get a list of lower case alphabets
+        tickList = list(string.ascii_lowercase)
+        # there are also the digits 1-9 in the ticker list
+        tickList += [ str(n) for n in range(1,10) ]
+        urlList = []
+        for ch in tickList:
+            urlList.append( self.baseUrl + "?ticker=" + ch )
+        return urlList
